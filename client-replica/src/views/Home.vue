f283d57f6a4533f940e8b808bd7ead82158a0824
valorad <valorad@outlook.com>
2019-10-01 19:18:48 -0400

    init replica front-end

---
+++ client-replica/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    Voila-CDN-Replica-Client works
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Home extends Vue {}
+</script>
---
+++ client-replica/vue.config.js
@@ -0,0 +1,36 @@
+// module.exports = {
+//   publicPath: '/statics'
+// }
+
+const { resolve } = require("path")
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const env = process.env.NODE_ENV;
+
+let config = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@/": resolve(__dirname, 'src/')
+      }
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        { 
+          from: resolve("./src", "statics"),
+          to: 'statics',
+          ignore: [
+            ".gitkeep"
+          ]
+        }
+      ])  
+    ]
+
+  }
+}
+
+if (env && env.toLowerCase() !== "development") {
+  config.publicPath = "/statics";
+}
+
+module.exports = config;
