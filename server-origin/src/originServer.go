6261e23ca7f313dd3dba6d4271c2887479dc253a
valorad <valorad@outlook.com>
2019-10-02 23:35:34 -0400

    origin-server: fix package name

--- server-origin/src/originServer.go
+++ server-origin/src/originServer.go
@@ -4,7 +4,7 @@ import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 
-	"github.com/valorad/voila-CDN/server-origin/routes"
+	"github.com/valorad/voila-CDN/server-origin/src/routes"
 )
 
 func main() {
