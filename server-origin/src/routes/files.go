e19c9100384e9fb1925b4d8e74f571a0c953a40e
valorad <valorad@outlook.com>
2019-10-07 11:11:32 -0400

    json serialization optimize

--- server-origin/src/routes/files.go
+++ server-origin/src/routes/files.go
@@ -43,26 +43,8 @@ func FileAdd(c echo.Context) error {
 		SetFile("document", "/mnt/m/workbench/_linux/voila-CDN/server-origin/statics/amd.txt").
 		Post("http://localhost:8000/api/files")
 
-	fmt.Println(resp.String())
-
-	// res := new(fileAddResult)
-	// if err := c.Bind(res); err != nil {
-	// 	return c.JSON(http.StatusInternalServerError, "Failed to show result")
-	// }
-
-	// result := echo.Map{}
-	// if err := c.Bind(&result); err != nil {
-	// 	return err
-	// }
-
-	// replicaResult := &ReplicaFileAddResult {
-	// 	OK: resp.Request.Body,
-	// 	Message 
-	// 	Url 
-	// }
-
 	var replicaResult ReplicaFileAddResult
-	json.Unmarshal([]byte(resp.String()), &replicaResult)
+	json.Unmarshal(resp.Body(), &replicaResult)
 
   result := &FileAddResult{
     Message:  `Pushed the file "amd.txt" to replica`,
