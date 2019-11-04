be62c0f0613a0ae4db839283a7b371b0c760e40f
valorad <valorad@outlook.com>
2019-11-04 10:40:17 -0500

    add vue material

--- client-replica/src/main.ts
+++ client-replica/src/main.ts
@@ -3,7 +3,43 @@ import App from "./App.vue";
 import router from "./router";
 import "./registerServiceWorker";
 
+import {
+  MdButton,
+  MdContent,
+  MdTabs,
+  MdApp,
+  MdDrawer,
+  MdToolbar,
+  MdList,
+  MdIcon,
+  MdTable,
+  MdField,
+  MdCard,
+  MdCheckbox,
+  MdDialog,
+  MdSteppers,
+  MdMenu,
+} from 'vue-material/dist/components';
+
 Vue.config.productionTip = false;
+
+// vue material
+Vue.use(MdButton);
+Vue.use(MdTabs);
+Vue.use(MdApp);
+Vue.use(MdDrawer);
+Vue.use(MdToolbar);
+Vue.use(MdList);
+Vue.use(MdIcon);
+Vue.use(MdTable);
+Vue.use(MdField);
+Vue.use(MdCard);
+Vue.use(MdCheckbox);
+Vue.use(MdDialog);
+Vue.use(MdSteppers);
+Vue.use(MdContent);
+Vue.use(MdMenu);
+
 
 new Vue({
   router,
